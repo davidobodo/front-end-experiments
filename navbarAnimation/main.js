b0a15b54dddf675a83a2815fc125a7dcd34a7c2e
David Obodo <obododavid5@gmail.com>
2020-06-19 02:52:41 +0100

    slide in navabr

--- navbarAnimation/main.js
+++ navbarAnimation/main.js
@@ -1,10 +1,35 @@
 const hamburger = document.querySelector('.hamburger')
+const cancel = document.querySelector('.cancel')
 const sidebar = document.querySelector('.sidebar')
 const links = document.querySelectorAll('.sidebar__links__link')
 
 const handleShowSidebar = () => {
-    sidebar.classList.toggle('showNavbar')
+    sidebar.classList.add('showNavbar')
+
+    links.forEach((element, index) => {
+        setTimeout(() => {
+            element.classList.add('fadeInLeft')
+        }, (100 * index) - (30 * index))
+    })
+}
+
+const handleHideSidebar = () => {
+
+    links.forEach((element, index) => {
+        setTimeout(() => {
+            element.classList.add('fadeOutRight')
+            // if (index == (links.length - 1)) {
+            //     console.log(index)
+            //     sidebar.classList.remove('showNavbar')
+            // }
+        }, ((links.length + 1) * 100) - (100 * index))
+    })
+
+    setTimeout(() => {
+
+    })
 }
 
 
 hamburger.addEventListener('click', handleShowSidebar)
+cancel.addEventListener('click', handleHideSidebar)
